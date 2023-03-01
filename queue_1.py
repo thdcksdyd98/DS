148f74d6da899b8f1b65c226f3be66ee0ed39183
thdcksdyd98 <thdcksdyd98@gmail.com>
2023-02-28 21:51:03 -0500

    stack/queue

--- queue_1.py
+++ queue_1.py
@@ -18,9 +18,22 @@ class queue():
     
     def enqueue(self,value):
         new_node = Node(value)
-        if self.length == 0:
-            return None
+        if self.first is None:
+            self.first = new_node
+            self.last = new_node
         else:
             self.last.next = new_node
             self.last = new_node
         self.length += 1
+
+    def dequeue(self): # pop_first
+        if self.length == 0:
+            return None
+        else:
+            target = self.first
+            self.first = self.first.next # self.first = None
+            target.next =  None
+            self.length -= 1
+            if self.length == 0:
+                self.last = None
+        return target
