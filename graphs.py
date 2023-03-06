0d51f11acc048ee9199c3a8a68792e05c89006c1
thdcksdyd98 <thdcksdyd98@gmail.com>
2023-03-06 09:03:56 -0500

    graphs

---
+++ graphs.py
@@ -0,0 +1,34 @@
+class Graph:
+    def __init__(self):
+        self.adj_list = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.add_vertex.keys():
+            self.adj_list[vertex] = []
+            return True
+        return False
+    
+    def add_edge(self, v1, v2): # two vertices
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            self.adj_list[v1].append(v2)
+            self.adj_list[v2].append(v1)
+            return True
+        return False
+    
+    def remove_edge(self, v1, v2):
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():
+            try:
+                self.adj_list[v1].remove(v2)
+                self.adj_list[v2].remove(v1)
+            except ValueError:
+                pass
+            return True
+        return False
+    
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():
+            for other_vertex in self.adj_list[vertex]:
+                self.adj_list[other_vertex].remove(vertex)
+            del self.adj_list[vertex]
+            return True
+        return False
