c929be74b03413f21fa70276bdc2ccfb6af94ab5
thdcksdyd98 <thdcksdyd98@gmail.com>
2023-03-10 11:31:12 -0500

    BasicSorts

---
+++ BasicSorts.py
@@ -0,0 +1,24 @@
+# bubble sort
+
+def bubble_sort(my_list):
+    for i in range(len(my_list)-1,0,-1):
+        for j in range(i):
+            if my_list[j] > my_list[j+1]:
+                tmp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = tmp
+    return my_list
+
+# selection sort
+
+def selelction_sort(my_list):
+    for i in range(len(my_list)-1):
+        min_index = i
+        for j in range(i+1, len(my_list)):
+            if my_list[j] < my_list[min_index]:
+                min_index = j
+        if i != min_index: # for the case where min_index is equal to i -> already sorted
+            tmp = my_list[i]
+            my_list[i] = my_list[min_index]
+            my_list[min_index] = tmp
+    return my_list
