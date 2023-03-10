0748b07031b2ce96c110b231db8674b7f8fc4b4d
thdcksdyd98 <thdcksdyd98@gmail.com>
2023-03-10 13:20:42 -0500

    QuickSort

---
+++ QuickSort.py
@@ -0,0 +1,24 @@
+def swap(my_list, index1, index2):
+    tmp = my_list[index1]
+    my_list[index1] = my_list[index2]
+    my_list[index2] = tmp
+
+# pivot function (helper function)
+def pivot(my_list, pivot_index, end_index):
+    swap_index = pivot_index
+
+    for i in range(pivot_index + 1, end_index + 1):
+        if my_list[i] < my_list[pivot_index]:
+            swap_index += 1
+            swap(my_list, swap_index, i)
+    swap(my_list, pivot_index, swap_index)
+    return swap_index
+
+def quick_sort(my_list, left, right):
+    if left < right:
+        pivot_index = pivot(my_list, left, right)
+        quick_sort(my_list, left, pivot_index-1)
+        quick_sort(my_list, pivot_index+1, right)
+    return my_list
+
+# time complexity = O(n^2) worst case
